86a9628b4c548220c347927a8f08c0c0f01292f2
JXiMePa <jurikfav0rit@gmail.com>
2018-05-25 18:17:17 +0300

    Always can do it better.

---
+++ WeatherStation. STORY BOARD/README.md
@@ -0,0 +1,7 @@
+# Weather Station Data.
+
+Simple rules to use this application:
+- Choose source of weather information from list that located on the bottom.
+- Browse full list of data by scrolling it up and down.
+- To sort by year you can click on "year" in the title.
+- Button "⇪» return you back to the top.
--- WeatherStation. STORY BOARD/WeatherHistoryViewController.swift
+++ WeatherStation. STORY BOARD/WeatherHistoryViewController.swift
@@ -13,31 +13,43 @@ final class WeatherHistoryViewController: UIViewController {
     private let baseURL = "https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/"
     private let cellId = "cellId"
     private let station = ["bradford", "yeovilton", "valley", "wickairport", "waddington", "whitby"]
-    private var dataSource: [StationData] = []
+    private var dataSource: [StationData] = [] {
+        didSet {  stationDataTableView.reloadData()  }
+    }
+    private var sortDataSwitcher = true
     
-    @IBOutlet weak var spiner: UIActivityIndicatorView!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var stationLabel: UILabel!
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var stationDataTableView: UITableView!
-    @IBOutlet weak var selectStationPickerView: UIPickerView!
-    @IBOutlet weak var infoButton: UIButton!
-    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet private weak var spiner: UIActivityIndicatorView!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var stationLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var stationDataTableView: UITableView!
+    @IBOutlet private weak var selectStationPickerView: UIPickerView!
+    @IBOutlet private weak var infoButton: UIButton!
+    @IBOutlet private weak var infoLabel: UILabel!
     
     // Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         spiner.startAnimating()
-  
-        selectStationPickerView.delegate = self
-        selectStationPickerView.dataSource = self
+
         imageView.image = #imageLiteral(resourceName: "Weather")
-        selectStationAtIndex(index: 0)
+        selectStationAt(index: 0)
         
         // configure tableView
         stationDataTableView.estimatedRowHeight = 30.0
         stationDataTableView.rowHeight = UITableViewAutomaticDimension
+    }
+  
+    //MARK: sortByYear
+    @IBAction private func sortByYearButton(_ sender: UIButton) {
+        if sortDataSwitcher {
+            dataSource = dataSource.sorted(by: ) {$0.year > $1.year}
+            sortDataSwitcher = !sortDataSwitcher
+        } else {
+            dataSource = dataSource.sorted(by: ) {$0.year < $1.year}
+            sortDataSwitcher = !sortDataSwitcher
+        }
     }
     
     override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -50,6 +62,9 @@ final class WeatherHistoryViewController: UIViewController {
         infoLabel.isHidden = !infoLabel.isHidden
     }
     
+    @IBAction private func toTopButton(_ sender: UIButton) {
+        stationDataTableView.setContentOffset(.zero, animated: true)
+    }
    
     private func alert(message: String, title: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
@@ -58,7 +73,7 @@ final class WeatherHistoryViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    private func selectStationAtIndex(index: Int) {
+    private func selectStationAt(index: Int) {
         DataRequest.shared.getStringFromUrl(stringUrl: "\(baseURL)\(station[index])data.txt") { [weak self] error, location, dataSourse in
             guard error == nil else {
                 self?.alert(message: "Please check your internet connection and try again.", title: "Error 😓")
@@ -85,6 +100,7 @@ extension WeatherHistoryViewController: UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? WeatherCell else { return WeatherCell() }
         let currentItem = dataSource[indexPath.item]
+        
         cell.setupCellValuesBy(itemData: currentItem)
         
         return cell
@@ -105,15 +121,14 @@ extension WeatherHistoryViewController: UIPickerViewDelegate, UIPickerViewDataSo
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         spiner.startAnimating()
         stationLabel.text = station[row].capitalized
-        selectStationAtIndex(index: row)
+        selectStationAt(index: row)
     }
     
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        return 30
+        return 30.0
     }
     
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
         return NSAttributedString(string: station[row].capitalized, attributes: [ .foregroundColor : #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1) ])
     }
 }
-
